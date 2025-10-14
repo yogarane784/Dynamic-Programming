db3afb9ed0e84314e27f4fb43c7d7f869b19e64c
yogarane784 <yoga.ranjan.singh@gmail.com>
2025-10-14 14:24:59 +0530

    Create SubsetSum.java

---
+++ SubsetSum.java
@@ -0,0 +1,60 @@
+package dp;
+
+/**
+ * Given an array arr[] of non-negative integers and a value sum, the task is to
+ * check if there is a subset of the given array whose sum is equal to the given
+ * sum.
+ * 
+ * Examples:
+ * 
+ * Input: arr[] = [3, 34, 4, 12, 5, 2], sum = 9 Output: True Explanation: There
+ * is a subset (4, 5) with sum 9.
+ * 
+ * Input: arr[] = [3, 34, 4, 12, 5, 2], sum = 30 Output: False Explanation:
+ * There is no subset that add up to 30.
+ * 
+ **/
+
+public class SubsetSum {
+
+	static Boolean dp[][];
+
+	public static void main(String args[]) {
+
+		int nums[] = { 3, 34, 4, 12, 5, 2 };
+		int sum = 9;
+		int n = nums.length;
+
+		dp = new Boolean[n + 1][sum + 1];
+		
+		for (int i = 0; i <= n; i++) {
+			dp[0][i] = false;
+		}
+
+		for (int i = 0; i <= n; i++) {
+			dp[i][0] = true;
+		}
+
+		System.out.println("Subset sum present : " + subsetSum(nums, sum, n));
+	}
+
+	static boolean subsetSum(int nums[], int sum, int n) {
+
+		if (n <= 0 && sum>0)
+			return false;
+
+		if (sum == 0)
+			return true;
+
+		if (dp[n][sum] != null)
+			return dp[n][sum];
+
+		if (nums[n - 1] <= sum)
+			return dp[n][sum] = subsetSum(nums, sum-nums[n-1], n-1) || subsetSum(nums, sum, n-1);
+
+		else
+			return dp[n][sum] = subsetSum(nums, sum, n-1);
+
+	}
+
+}
